bec84d772a170d15559c3c6f658e9eceb5fe0e84
Igor Radkevich <landrommigor@gmail.com>
2023-12-18 15:08:06 +0300

    Добавлен пример алгоритма шиброваня "АТБАШ"

---
+++ AtbashEncrypt/Atbash.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Атбаш шифрование.
+public class Atbash
+{
+	//Алфавит языка:
+	private const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+	//Метод для переворачивания строки:
+	private string Reverse(string inputText)
+	{
+		var reversedText = string.Empty;	//Переменная для хранения результата:
+		foreach (var s in inputText)
+			reversedText = s + reversedText;//Добавляем символ в начало строки:
+
+		return reversedText;
+	}
+
+	//Метод шифрования/дешифрования:
+	private string EncryptDecrypt(string text, string symbols, string cipher)
+	{
+		//Переводим текст в нижний регистр:
+		text = text.ToLower();
+
+		var outputText = string.Empty;
+		for (var i = 0; i < text.Length; i++)
+		{
+			//Поиск позиции символа в строке алфавита:
+			var index = symbols.IndexOf(text[i]);
+			if (index >= 0)
+				//Замена символа на шифр:
+				outputText += cipher[index].ToString();
+		}
+		return outputText;
+	}
+
+	//Шифрование текста:
+	public string EncryptText(string plainText)
+	{
+		return EncryptDecrypt(plainText, alphabet, Reverse(alphabet));
+	}
+
+	//Расшифровка текста:
+	public string DecryptText(string encryptedText)
+	{
+		return EncryptDecrypt(encryptedText, Reverse(alphabet), alphabet);
+	}
+}
---
+++ AtbashEncrypt/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Атбаш шифрование.");
+var atbash = new Atbash();
+
+Console.Write("Введите текст сообщения: ");
+var message = Console.ReadLine();
+var encryptedMessage = atbash.EncryptText(message!);
+
+Console.WriteLine("Зашифрованное сообщение: {0}", encryptedMessage);
+var decryptedMessage = atbash.DecryptText(encryptedMessage);
+
+Console.WriteLine("Расшифрованное сообщение: {0}", decryptedMessage);
+Console.ReadLine();
